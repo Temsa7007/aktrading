f907be01446e0b95d01ce0b4533da398a95b319c
yousef hijazi <yousef.yh.2001@gmail.com>
2024-08-01 17:39:18 +0300

    remove request limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api', 'throttle:200,.10',
-            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api', 'throttle:10000,60', // Allow 10000 requests per hour
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api', 'throttle:999999999,60', // Allow 10000 requests per hour
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\JsonMiddleware::class,
         ],
